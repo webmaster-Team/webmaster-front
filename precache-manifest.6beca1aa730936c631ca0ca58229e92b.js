ac447ef42a87435ce2fa985be2d154d933632c78
solingjees (from Travis CI) <1600346867@qq.com>
2020-04-26 03:10:34 +0000

    Deploy webmaster-Team/webmaster-front to github.com/webmaster-Team/webmaster-front.git:gh-pages

---
+++ precache-manifest.6beca1aa730936c631ca0ca58229e92b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "54c7aa252c4ce37c5c89ce7c46fb0348",
+    "url": "/index.html"
+  },
+  {
+    "revision": "46644f8a1a01135df14e",
+    "url": "/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "1d68e70b14917959d732",
+    "url": "/static/js/2.0c0dcd9b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.0c0dcd9b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "46644f8a1a01135df14e",
+    "url": "/static/js/main.c79a9be4.chunk.js"
+  },
+  {
+    "revision": "6a649bfccdc648d6cfad",
+    "url": "/static/js/runtime-main.36ca0f5b.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
