aa0c157bbf95d6ee0ae37c31db06cd35bcda1f54
苏灵杰 <sulingjie@sulingjiedeMacBook-Pro.local>
2020-05-25 15:04:58 +0800

    测试axios的全局配置能否成功修改baseurl头

--- src/config/index.js
+++ src/config/index.js
@@ -6,11 +6,12 @@ let baseUrl = {
   pro: 'http://123.56.3.135:8080', //服务器
 }
 
+
 export default {
   //axios配置
   baseUrl,
   fileUploadBaseUrl: baseUrl.dev,
   // process.env.NODE_ENV === 'development' ? baseUrl.dev : baseUrl.pro,
   captchaBaseUrl: baseUrl.dev,
-  // process.env.NODE_ENV === 'development' ? "123.56.3.135:8080/" : "http://172.26.51.186:8082"
+  // process.env.NODE_ENV === 'development' ? "" : "http://172.26.51.186:8082"
 }
