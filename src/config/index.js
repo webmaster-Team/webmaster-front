882057fbd28a273af8a803f2cbbbbe24536b4d46
苏灵杰 <sulingjie@sulingjiedeMacBook-Pro.local>
2020-05-25 15:16:14 +0800

    添加cross-env用于处理环境切换的问题

--- src/config/index.js
+++ src/config/index.js
@@ -2,7 +2,7 @@ let baseUrl = {
   // dev: 'http://localhost:36742/',
   // dev: 'http://172.26.51.237:8080',
   // dev: 'http://192.168.43.111:8080',
-  dev: 'http://123.56.3.135:8080',
+  dev: 'http://123.56.3.135:8080', //服务器的接口已经完成，可以直接调用
   pro: 'http://123.56.3.135:8080', //服务器
 }
 
@@ -11,7 +11,5 @@ export default {
   //axios配置
   baseUrl,
   fileUploadBaseUrl: baseUrl.dev,
-  // process.env.NODE_ENV === 'development' ? baseUrl.dev : baseUrl.pro,
   captchaBaseUrl: baseUrl.dev,
-  // process.env.NODE_ENV === 'development' ? "" : "http://172.26.51.186:8082"
 }
--- src/utils/request.js
+++ src/utils/request.js
@@ -1,11 +1,10 @@
 import HttpRequest from './axios'
 import config from '../config'
 const baseUrl =
-  process.env.NODE_ENV === 'development'
-    ? config.baseUrl.dev
-    : config.baseUrl.pro
-//输出当前axios的头部
-console.log("axios的baseurl为"+baseUrl)
+  process.env.NODE_ENV === 'development' ?
+  config.baseUrl.dev :
+  config.baseUrl.pro
+
 const axios = new HttpRequest(baseUrl)
 
 export default axios
