0eb0d39e4fc6146f38e6ea12924bfb44d0b96d81
苏灵杰 <sulingjie@sulingjiedeMacBook-Pro.local>
2020-05-25 14:25:37 +0800

    test aixos header 第三次

--- src/utils/request.js
+++ src/utils/request.js
@@ -4,6 +4,7 @@ const baseUrl =
   process.env.NODE_ENV === 'development'
     ? config.baseUrl.dev
     : config.baseUrl.pro
+//输出当前axios的头部
 console.log("axios的baseurl为"+baseUrl)
 const axios = new HttpRequest(baseUrl)
 
