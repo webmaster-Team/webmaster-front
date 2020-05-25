eb88953ef15c638c12e6f0255c3f206f03b4eb08
苏灵杰 <sulingjie@sulingjiedeMacBook-Pro.local>
2020-05-25 14:29:18 +0800

    printout axios baseurl

--- src/utils/request.js
+++ src/utils/request.js
@@ -4,6 +4,7 @@ const baseUrl =
   process.env.NODE_ENV === 'development'
     ? config.baseUrl.dev
     : config.baseUrl.pro
+// 修改axios的header部分
 console.log("axios的baseurl为"+baseUrl)
 const axios = new HttpRequest(baseUrl)
 
